6c8a86c1d1425b0646c8956eaab491a9dbea46a7
Dominic Truchon <domv.t.98@gmail.com>
2026-09-07 16:41:42 -0400

    feat: init - set initial cube state (solved)

--- RubiksCube.py
+++ RubiksCube.py
@@ -7,4 +7,11 @@ class RubiksCube:
         https://kociemba.org/math/CubeDefs.htm - Move definitions
     """
 
+    def __init__(self):
+        # Solved cube
+        self.corner_position = np.arange(8, dtype=np.int8) # arr length 8, val 0-7
+        self.corner_orientation = np.zeros(8, dtype=np.int8) # arr length 8, val 0s
+        self.edge_position = np.arange(12, dtype=np.int8) # arr length 12, val 0-11
+        self.edge_orientation = np.zeros(12, dtype=np.int8) # arr length 12, val 0s
 
+    
